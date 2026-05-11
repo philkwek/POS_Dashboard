226ce2f1ea54e3cfd43dbad2877ec9c9bc9202bd
philkwek <philkwek03@gmail.com>
2026-05-11 17:06:51 +0800

    Add new typing for variants sub-collection

--- shared/types/Store.ts
+++ shared/types/Store.ts
@@ -1,8 +1,15 @@
 export interface StoreItemType {
     attributes: Array<String>;
+    variants: Array<StoreItemVariantType>;
     basePrice: Number;
     description: string;
     imageURL: string;
     isActive: boolean;
     name: string;
+}
+
+export interface StoreItemVariantType {
+    name: string;
+    priceModifier: Number;
+    stock: Number;
 }
