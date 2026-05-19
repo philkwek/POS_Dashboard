eb165ce3365b166c4d482f9a70a2db70cae495f2
philkwek <philkwek03@gmail.com>
2026-05-19 22:04:40 +0800

    feat: Added number input and add to cart button

--- src/pages/StoreItemPage.tsx
+++ src/pages/StoreItemPage.tsx
@@ -158,15 +158,43 @@ function StoreItemPage() {
               <h1 className="text-base">{"$" + product.basePrice}</h1>
               <p>{product.description}</p>
               {variants.length > 0 && (
-                <select className="select mt-1 select-bordered w-full justify-self-stretch" defaultValue="Variants">
+                <select
+                  className="select mt-1 select-bordered w-full justify-self-stretch"
+                  defaultValue="Variants"
+                >
                   <option disabled={true}>Variants</option>
                   {variants.map((variant, index) => (
                     <option key={index} value={variant.name}>
-                      {variant.name} {variant.priceModifier ? `(+$${variant.priceModifier})` : ""}
+                      {variant.name}{" "}
+                      {variant.priceModifier
+                        ? `(+$${variant.priceModifier})`
+                        : ""}
                     </option>
                   ))}
                 </select>
               )}
+              <div className="flex flex-col">
+                <input
+                  type="number"
+                  className="input validator justify-self-stretch w-full"
+                  required
+                  placeholder="Quantity"
+                  min="1"
+                  max="10"
+                  title="Must be between be 1 to 10"
+                />
+              </div>
+              <button className="btn btn-lg flex flex-row gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-6"
+                >
+                  <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+                </svg>
+                Add to Cart
+              </button>
             </div>
           </div>
         ) : (
